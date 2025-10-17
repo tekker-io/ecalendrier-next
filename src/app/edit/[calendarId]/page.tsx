263e26adb8fb44c53f9961818e4329f6d0c9d02f
Arnaud Drain <arnaud@tekker.io>
2025-10-17 11:55:07 +0200

    push duration logs

--- src/app/edit/[calendarId]/page.tsx
+++ src/app/edit/[calendarId]/page.tsx
@@ -8,10 +8,18 @@ export default async function CalendarPage({
 }: {
   params: { calendarId: string };
 }) {
+  const beforeGetUserFromCookie = Date.now();
   const user = await getUserFromCookie();
+  console.log(
+    `getUserFromCookie took ${Date.now() - beforeGetUserFromCookie}ms`
+  );
+  const beforeGetParams = Date.now();
   const { calendarId } = await params;
+  console.log(`Getting params took ${Date.now() - beforeGetParams}ms`);
+  const beforeDbFetch = Date.now();
   const db = admin.firestore();
   const doc = await db.collection("calendars").doc(calendarId).get();
+  console.log(`DB fetch took ${Date.now() - beforeDbFetch}ms`);
   const calendar: Calendar | undefined = !doc.exists
     ? undefined
     : {
