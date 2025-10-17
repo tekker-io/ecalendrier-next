f14e3eaf1aafb0a2343dfe6a2a254970fd6b4df6
Arnaud Drain <arnaud@tekker.io>
2025-10-17 15:51:26 +0200

    configure favicon and icon and all

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,6 +9,9 @@ import "./globals.css";
 export const metadata: Metadata = {
   title: "eCalendrier - Calendrier de l'avent en ligne",
   description: "eCalendrier",
+  openGraph: {
+    images: "/bg.webp",
+  },
 };
 
 const roboto = Roboto({
